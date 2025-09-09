a50a4f9e1e1bd8ee459a3b3a75e90941aa402201
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-09-09 13:19:23 -0400

    Changed 'Hello, World!' to 'Hello, Jack!'

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Sep 2025
+ * This program says 'Hello, Jack!'
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Jack!')
